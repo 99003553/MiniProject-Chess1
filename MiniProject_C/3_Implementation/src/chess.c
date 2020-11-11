f653faadba5feceee49af4cbcde6103de39d5587
Alen Reji Kokkad <69103817+stepin104484@users.noreply.github.com>
2020-11-11 12:43:57 +0530

    Update chess.c

--- MiniProject_C/3_Implementation/src/chess.c
+++ MiniProject_C/3_Implementation/src/chess.c
@@ -7,11 +7,11 @@
 int horse(int x,int y,int x1,int y1)
 {
 	return 1;
-	int a=0;
+	/*int a=0;
 	if((y1==y+2 && x1==x+1)||(y1==y+2 && x1==x-1)||(y1==y+1 && x1==x+2)||(y1==y+1 && x1==x-2)||(y1==y-1 && x1==x+2)||(y1==y-1 && x1==x-2)||(y1==y-2 && x1==x+1)||(y1==y-2 && x1==x-1)){
 		a = 1;
 		}
-	return a;
+	return a;*/
 }
 
 
